08a8981cbabffb05c8260dfd55f1c292f6c36eae
Mas_andara <masandaraputra123@gmail.com>
2024-10-31 10:07:44 +0700

    Update Folder Permainan & New File Image

--- Mempelajari/main_Menyusunsk.py
+++ Mempelajari/main_Menyusunsk.py
@@ -1,12 +1,91 @@
-from kivy.uix.screenmanager import Screen
+import random
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-from kivy.uix.screenmanager import SlideTransition, FadeTransition, ScreenManager, Screen
+from kivy.uix.screenmanager import (
+    SlideTransition,
+    FadeTransition,
+    ScreenManager,
+    Screen,
+)
+from kivy.animation import Animation
+
 
 class MenyusunSKScreen(Screen):
-     def go_to_mempelajari(self):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.jawaban_benar = ""
+        self.jawaban_terpilih = []
+        self.soal_list = [
+            {"gambar": "Image/Susun-kata/Mobil.png", "jawaban": ["MO", "BIL"]},
+            {"gambar": "Image/Susun-kata/Sepeda.png", "jawaban": ["SE", "PE", "DA"]},
+            {"gambar": "Image/Susun-kata/Motor.png", "jawaban": ["MO", "TOR"]},
+            {"gambar": "Image/Susun-kata/Perahu.png", "jawaban": ["PE", "RA", "HU"]},
+            {"gambar": "Image/Susun-kata/Becak.png", "jawaban": ["BE", "CAK"]},
+            {
+                "gambar": "Image/Susun-kata/Helikopter.png",
+                "jawaban": ["HELI", "KOP", "TER"],
+            },
+            # Tambahkan soal lainnya sesuai kebutuhan
+        ]
+        self.load_random_soal()
+
+    def on_enter(self):
+        # Randomisasi soal setiap kali layar diakses
+        self.load_random_soal()
+
+    def load_random_soal(self):
+        # Mengambil soal secara acak
+        soal = random.choice(self.soal_list)
+        self.ids.gambar_utama.source = soal["gambar"]
+        self.jawaban_benar = soal["jawaban"]
+        self.jawaban_terpilih = []
+
+        # Mengacak letak jawaban
+        jawaban_acak = random.sample(soal["jawaban"], len(soal["jawaban"]))
+
+        # Menampilkan opsi jawaban secara acak
+        for i, box in enumerate(self.ids.opsi_jawaban.children):
+            if i < len(jawaban_acak):
+                box.children[0].text = jawaban_acak[i]
+            else:
+                box.children[0].text = ""
+
+    def pilih_jawaban(self, teks):
+        # Memasukkan jawaban ke dalam jawaban yang dipilih jika belum lengkap
+        if len(self.jawaban_terpilih) < len(self.jawaban_benar):
+            self.jawaban_terpilih.append(teks)
+
+            # Memperbarui label jawaban di bawah gambar
+            jawaban_label = "     ".join(self.jawaban_terpilih)
+            self.ids.label_jawaban.text = jawaban_label
+
+            # Animasi saat jawaban benar
+            selected_label = Label(text=teks, font_size="30sp", color=(0, 0, 0, 1))
+            self.ids.float_layout.add_widget(
+                selected_label
+            )  # Tambahkan widget label sementara
+
+            anim = Animation(
+                center_y=self.ids.label_jawaban.center_y, d=0.5, t="out_bounce"
+            )
+            anim.start(selected_label)
+
+            # Cek apakah jawaban sudah lengkap dan benar
+            if len(self.jawaban_terpilih) == len(self.jawaban_benar):
+                if self.jawaban_terpilih == self.jawaban_benar:
+                    # Tunggu sebentar sebelum lanjut ke soal berikutnya
+                    anim.bind(on_complete=lambda *args: self.load_random_soal())
+                else:
+                    # Reset jawaban jika salah
+                    self.jawaban_terpilih = []
+                    self.ids.label_jawaban.text = "----     ----     ----"
+
+    def go_to_mempelajari(self):
         # Navigate back to MempelajariScreen with a fade transition
         self.manager.transition = SlideTransition(direction="left", duration=0.2)
-        self.manager.current = 'mempelajari'
+        self.manager.current = "mempelajari"
+
 
 # Load the Alfabet.kv file manually
-Builder.load_file('Mempelajari/menyusunsk.kv')
+Builder.load_file("Mempelajari/menyusunsk.kv")
--- Permainan/main_Tebakwarna.py
+++ Permainan/main_Tebakwarna.py
@@ -1,4 +1,9 @@
-from kivy.uix.screenmanager import SlideTransition, FadeTransition, ScreenManager, Screen
+from kivy.uix.screenmanager import (
+    SlideTransition,
+    FadeTransition,
+    ScreenManager,
+    Screen,
+)
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
@@ -8,12 +13,143 @@ from kivy.uix.image import Image
 from kivy.properties import StringProperty, ListProperty
 from kivy.animation import Animation
 import random
+from kivy.clock import Clock
+
 
 class TebakWarnaScreen(Screen):
+    current_question = 0
+
+    def on_enter(self):
+        # Langsung memuat pertanyaan tanpa delay
+        self.load_question()
+
+    def load_question(self):
+        # Define the questions and answers
+        questions = [
+            {
+                "question": "MERAH",
+                "answers": [
+                    ("Merah", [1, 0, 0, 1]),
+                    ("Hijau", [0, 1, 0, 1]),
+                    ("Biru", [0, 0, 1, 1]),
+                ],
+            },
+            {
+                "question": "BIRU",
+                "answers": [
+                    ("Biru", [0, 0, 1, 1]),
+                    ("Merah", [1, 0, 0, 1]),
+                    ("Kuning", [1, 1, 0, 1]),
+                ],
+            },
+            {
+                "question": "HIJAU",
+                "answers": [
+                    ("Hijau", [0, 1, 0, 1]),
+                    ("Hitam", [0, 0, 0, 1]),
+                    ("Putih", [1, 1, 1, 1]),
+                ],
+            },
+            {
+                "question": "ORANGE",
+                "answers": [
+                    ("Orange", [1, 0.5, 0, 1]),
+                    ("Ungu", [0.5, 0, 0.5, 1]),
+                    ("Biru", [0, 0, 1, 1]),
+                ],
+            },
+            {
+                "question": "PUTIH",
+                "answers": [
+                    ("Putih", [1, 1, 1, 1]),
+                    ("Merah", [1, 0, 0, 1]),
+                    ("Hijau", [0, 1, 0, 1]),
+                ],
+            },
+            {
+                "question": "HITAM",
+                "answers": [
+                    ("Hitam", [0, 0, 0, 1]),
+                    ("Kuning", [1, 1, 0, 1]),
+                    ("Ungu", [0.5, 0, 0.5, 1]),
+                ],
+            },
+            {
+                "question": "KUNING",
+                "answers": [
+                    ("Kuning", [1, 1, 0, 1]),
+                    ("Orange", [1, 0.5, 0, 1]),
+                    ("Hijau", [0, 1, 0, 1]),
+                ],
+            },
+            {
+                "question": "UNGU",
+                "answers": [
+                    ("Ungu", [0.5, 0, 0.5, 1]),
+                    ("Putih", [1, 1, 1, 1]),
+                    ("Merah", [1, 0, 0, 1]),
+                ],
+            },
+        ]
+
+        # Pick a random question for the current session
+        question_data = random.choice(questions)
+        self.ids.question_label.text = question_data["question"]
+
+        # Update button text and color for answers
+        for i, answer_data in enumerate(question_data["answers"]):
+            button = self.ids[f"answer_button_{i+1}"]
+            button.text = answer_data[0]
+            button.background_color = answer_data[1]
+
+    def check_answer(self, selected_answer):
+        # Define correct answer for each color
+        correct_answers = {
+            "MERAH": "Merah",
+            "BIRU": "Biru",
+            "HIJAU": "Hijau",
+            "ORANGE": "Orange",
+            "PUTIH": "Putih",
+            "HITAM": "Hitam",
+            "KUNING": "Kuning",
+            "UNGU": "Ungu",
+        }
+
+        # Check if the selected answer is correct
+        correct_answer = correct_answers.get(self.ids.question_label.text, "")
+        if selected_answer == correct_answer:
+            self.show_popup("Jawaban Benar!")
+        else:
+            self.show_popup("Jawaban Salah!")
+
+    def show_popup(self, message):
+        # Display popup message
+        popup_content = BoxLayout(orientation="vertical")
+        popup_content.add_widget(Label(text=message, font_size="30sp"))
+
+        popup = Popup(
+            title="", content=popup_content, size_hint=(0.6, 0.4), auto_dismiss=False
+        )
+        popup.open()
+
+        # Set random delay for popup close and move to the next question
+        random_popup_delay = random.uniform(
+            2, 4
+        )  # Random delay between 2 and 4 seconds
+        Clock.schedule_once(lambda dt: self.next_question(popup), random_popup_delay)
+
+    def next_question(self, popup):
+        # Dismiss popup
+        popup.dismiss()
+
+        # Load a new question
+        self.load_question()
+
     def go_to_permainan(self):
         # Navigate back to MempelajariScreen with a fade transition
         self.manager.transition = SlideTransition(direction="left", duration=0.2)
-        self.manager.current = 'permainan'
-        
+        self.manager.current = "permainan"
+
+
 # Load the Alfabet.kv file manually
-Builder.load_file('Permainan/tebakwarna.kv')
+Builder.load_file("Permainan/tebakwarna.kv")
